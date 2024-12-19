34c6d103b3270d3e69b0a8896576db9d94c1702d
jmansdorfer <jmansdorfer19@gmail.com>
2024-12-18 23:02:52 -0500

    adjusting project structure

--- __init__.py
+++
@@ -1,4 +0,0 @@
-"""Create controlled and compliant AI systems with PredictionGuard and LangChain"""
-from .src import ChatPredictionGuard, PredictionGuard, PredictionGuardEmbeddings
-
-__version__ = "0.1.0"
---
+++ langchain_predictionguard/__init__.py
@@ -0,0 +1,6 @@
+"""Create controlled and compliant AI systems with PredictionGuard and LangChain"""
+from ChatPredictionGuard import ChatPredictionGuard
+from Predictionguard import PredictionGuard
+from PredictionGuardEmbeddings import PredictionGuardEmbeddings
+
+__version__ = "0.1.0"
