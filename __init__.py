91617c834c33d883ff5b46ee4f08413c5bc0fa71
jmansdorfer <jmansdorfer19@gmail.com>
2024-12-18 22:55:58 -0500

    initial code

---
+++ __init__.py
@@ -0,0 +1,4 @@
+"""Create controlled and compliant AI systems with PredictionGuard and LangChain"""
+from .src import ChatPredictionGuard, PredictionGuard, PredictionGuardEmbeddings
+
+__version__ = "0.1.0"
