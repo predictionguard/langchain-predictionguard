02894d2bbd059bdf27fa2db3ea924e79ab78a038
jmansdorfer <jmansdorfer19@gmail.com>
2025-05-13 16:19:13 -0400

    adding rerank component

---
+++ tests/test_PredictionGuardRerank.py
@@ -0,0 +1,16 @@
+"""Test Prediction Guard API wrapper"""
+
+from langchain_core.documents import Document
+
+from langchain_predictionguard import PredictionGuardRerank
+
+
+def test_langchain_cohere_rerank_documents() -> None:
+    rerank = PredictionGuardRerank(model="bge-reranker-v2-m3")
+    test_query = "Test query"
+    test_documents = [
+        "This is a test document.",
+        "Another test document."
+    ]
+    results = rerank.rerank(query=test_query, documents=test_documents)
+    assert len(results) == 2
